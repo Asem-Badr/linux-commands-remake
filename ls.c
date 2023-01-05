6740c7e6ab744c22ae250b14cce932aa6a52ff67
Asem-Badr <70919962+Asem-Badr@users.noreply.github.com>
2023-01-05 19:38:44 +0000

    making the basic function of ls work

--- ls.c
+++ ls.c
@@ -1,9 +1,27 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+
 
 int main(void)
 
 {
-    printf("hello world ! \n");
+    DIR *directory;
+    struct dirent *files;
+    int files_number = 0;
+    directory =opendir(".");
+    if(directory == NULL)
+    {
+        printf("unable to read this directory ");
+        return 0;
+    }
+    while(files = readdir(directory))
+    {
+        files_number ++;
+        printf("%s \n",files->d_name);
+    }
+
+    closedir(directory);
     return 0;
 }
