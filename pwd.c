b1c987180ab32a33a0141bc6e970a9f8d00cfb28
Asem-Badr <70919962+Asem-Badr@users.noreply.github.com>
2023-01-06 11:57:39 +0000

    adding pwd command

---
+++ pwd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+    char *buf;
+    buf=(char *)malloc(100*sizeof(char));
+    getcwd(buf,100);
+    printf("\n %s \n",buf);
+    return 0;
+}
