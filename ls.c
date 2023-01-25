c90a58df34010390e5969d7c9315ede045cb3fa2
Asem-Badr <70919962+Asem-Badr@users.noreply.github.com>
2023-01-25 16:08:16 +0000

    making ls command handle different directories other than .

--- ls.c
+++ ls.c
@@ -5,18 +5,26 @@
 #include <string.h>
 #define true 1
 #define false 0
-void check_options(int *a, int *f, int *la, int *lh, int *argc, char **argv);
+
+void check_options(int *a, int *f, int *la, int *lh, int argc, char **argv,char**buffer);
 int compareTwoString(char *a, char *b);
 int main(int argc ,char **argv)
 
 {
     DIR *directory;
-    int a=0,f=0,la=0,lh=0;
-    //check_options(&a ,&f ,&la, &lh, &argc ,argv);
-
+    int a=0,f=0,la=0,lh=0;//options flags
+    char* buffer = NULL;
+    check_options(&a ,&f ,&la, &lh, argc ,argv ,&buffer);
+    //printf("\n\n%s\n\n",buffer); for debugging purposes
     struct dirent *files;
     int files_number = 0;
-    directory =opendir(".");
+    if(buffer == NULL)
+    {
+        directory = opendir(".");
+    }else
+    {
+        directory = opendir(buffer);
+    }
     /*the opendir function opens a directory stream related to the directory 
     name and returns a pointer to the directory stream 
     the stream is positioned at the first entry in the directory*/
@@ -36,22 +44,41 @@ int main(int argc ,char **argv)
         printf("%s \t",files->d_name);
     }
     //the following lines are for debugging purpose only
-    printf("%d %d %d %d",a,f,la,lh);
-    printf("%d",strcmp(argv[1],"f"));
+    /*
+    printf("%d %d %d %d/n",a,f,la,lh);
+    printf("%s",buffer);
+    */
+
+    //printf("%d",strcmp(argv[1],"f"));
     //************************************
     printf("\n");
     closedir(directory);
     return 0;
 }
 
-void check_options(int *a, int *f, int *la, int *lh, int *argc, char **argv)
+void check_options(int *a, int *f, int *la, int *lh, int argc, char **argv ,char**buffer)
 {
-    for(int i=0; i< *argc ;i++)
-    {   
-        if (!compareTwoString(argv[i],"a"))
-        *a = 1;
-        else if (!compareTwoString(argv[i],"f"))
+    for(int i=1; i< argc ;i++)
+    {   if (!compareTwoString(argv[i],"-a"))
+        {*a = true;
+        //the following lines are for debugging purposes only
+        /*printf("\n%d\n",i);
+        printf("%c\n",argv[1][0]);*/
+        }
+        else if (!compareTwoString(argv[i],"-f"))
         *f =true;
+        else if (!compareTwoString(argv[i],"-la"))
+        *la = true;
+        else if (!compareTwoString(argv[i],"-lh"))
+        *lh = true;
+        else if ((argv[i][0] != '-')&&(argv[i][0] != '.'))//not a flag nor . then it's a directory
+        {*buffer = argv[i];}
+        //printf("\n\n%s\n\n",*buffer);}
+        else 
+        {
+        printf("unknown option");
+        exit(1);
+        }
     }
     return;
 }
