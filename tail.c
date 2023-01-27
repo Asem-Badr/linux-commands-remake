2fcfca9a26c9b4ba3ed5b80e36d620050ff60072
Asem-Badr <asembadr200@gmail.com>
2023-01-27 14:52:45 +0200

    making tail command works

--- tail.c
+++ tail.c
@@ -4,15 +4,29 @@
 int main(int argc, char **argv)
 {
     FILE * file ;
+    int line_count =10 ,char_count =0; //will be flexible soon
     char buffer[buffer_size];
     file = fopen(one_arg(argc,argv),"r");
+    int beginning = ftell(file);
     fseek(file,0,SEEK_END);
-    //for(int i = 10 ; i>0 ; i--)
+    char ch ;
+    //the second part of the condition handles the case of 
+    //the file being shorter than 10 lines
+    while (line_count &&ftell(file)!=beginning)
     {
-        fgets(buffer,buffer_size,file);
-        
+        ch =getc(file);
+        if(ch =='\n')
+        {
+            line_count --;
+        }
+        char_count --;
+        fseek(file,char_count,SEEK_END);
     }
-    printf("%ld",ftell(file));
+    fseek(file,char_count+2,SEEK_END);
+    while(fgets(buffer,buffer_size,file)!=NULL)
+    {
+        printf("%s",buffer);
+    }
     fclose(file);
 
     return 0;
