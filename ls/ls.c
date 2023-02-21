9745e0e040d3b7115ffeacfb2fb1f9afb80ad060
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-21 20:57:21 +0200

    feat(ls): add ls program's main structure

---
+++ ls/ls.c
@@ -0,0 +1,10 @@
+#include "fun.h"
+
+int main(int argc, char **argv)
+{
+  // TODO:
+  // 1. Parse the command to get the options.
+  // 2. Parse the command to get the files.
+  // 3. Print the desired files with the required formatting.
+  return 0;
+}
